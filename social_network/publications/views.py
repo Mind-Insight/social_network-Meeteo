4a51022117b7335414a6f699b2da5b7981e0374d
Timofey <nsk.timka2077@gmail.com>
2023-05-06 16:49:18 +0200

    add sample publications and a little customized publications page with scss

--- social_network/publications/views.py
+++ social_network/publications/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+publications = [
+    {
+        id: 0,
+        'title': 'First publication',
+        'main': 'hello everyone! I make social network!',
+        'description': 'this is my first publication',
+        'date': '06-05-2023',
+        'likes': 0,
+    },
+    {
+        id: 1,
+        'title': 'Dogs',
+        'main': 'Dogs are really cute and necessary. You have to take care of them!',
+        'description': 'dog post',
+        'date': '12-06-2023',
+        'likes': 0,
+    }
+]
+
+
 def index(request):
-    return render(request, 'publications/index.html')
+    context = {
+        'publications': publications,
+        'title': 'Publications',
+    }
+    return render(request, 'publications/index.html', context)
