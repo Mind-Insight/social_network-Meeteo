079ce3420ba92739b3c55a5dc8ae56da242ccc71
Timofey <nsk.timka2077@gmail.com>
2023-05-09 13:00:34 +0200

    change 'publications/' url to '/'

--- social_network/social_network/urls.py
+++ social_network/social_network/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('publications/', include('publications.urls')),
+    path('', include('publications.urls')),
     path('chats/', include('chats.urls')),
     path('about/', include('about.urls')),
     path('profile/', include('user_profile.urls')),
